114fb02ecfa69a03eb1d4c1df15548ce79d7e531
LuisQuiros <luisangelquirosflorez@gmail.com>
2023-11-29 17:59:34 -0500

    Boton de columna y fila

--- src/components/pages/showProducts.js
+++ src/components/pages/showProducts.js
@@ -4,39 +4,73 @@ import ProductFetcher from '../ProductFetcher';
 
 const ShowProducts = () => {
   const [products, setProducts] = useState([]);
+  const [layout, setLayout] = useState(''); // Estado para el diseño actual
+
+  const row = () => {
+    setLayout(''); // Cambia al diseño en fila
+  };
+
+  const column = () => {
+    setLayout('column'); // Cambia al diseño en columna
+  };
 
   return (
     <div>
       <ProductFetcher setData={setProducts} />
-      <section className="container">
-      <article className="product-container">
+      
+      <div className='container--buttons'>
+      <button className='buttonColumn' onClick={row}></button>
+      <button className='buttonRow' onClick={column}></button>
+      </div>
+
+      <section className={`container${layout}`}>
+
+      <article className={`product-container${layout}`}>
           <img src="samsung.jpg" className='img' alt="Producto Samsung" />
-          <p>Desde:</p>
-          <h2>$500.000</h2>
           <h3>Nombre Producto</h3>
+          <div>
+          <p>Desde:</p>
+          </div>
+          <h2>$500.000</h2>
+       
+          
+          
         </article>
 
-        <article className="product-container">
+        <article className={`product-container${layout}`}>
           <img src="samsung.jpg" className='img' alt="Producto Samsung" />
-          <p>Desde:</p>
-          <h2>$500.000</h2>
           <h3>Nombre Producto</h3>
+          <div>
+          <p>Desde:</p>
+          </div>
+          <h2>$500.000</h2>
+       
+          
+          
         </article>
 
 
-        <article className="product-container">
+        <article className={`product-container${layout}`}>
           <img src="samsung.jpg"  className='img' alt="Producto Samsung" />
-          <p>Desde:</p>
-          <h2>$500.000</h2>
           <h3>Nombre Producto</h3>
+          <div>
+          <p>Desde:</p>
+          </div>
+          <h2>$500.000</h2>
+       
+          
+          
         </article>
 
         {products.map((product) => (
-          <article className="product-container" key={product.id}>
+          <article className={`product-container${layout}`} key={product.id}>
             <img src={product.image} className='img' alt={product.title} />
-            <p>Desde:</p>
-            <h2>${product.price}</h2>
+
             <h3>{product.title}</h3>
+            <p>Desde:</p>             
+            <h2>${product.price}</h2>    
+            
+            
           </article>
         ))}
 
